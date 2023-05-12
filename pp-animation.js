1d85490bded315c3b3732d1780d5e75e14b1ead3
SantiagoMtz25 <santimtzv01@gmail.com>
2023-05-12 11:30:46 -0600

    Initial add of files

---
+++ pp-animation.js
@@ -0,0 +1,35 @@
+new FinisherHeader({
+    "count": 100,
+    "size": {
+        "min": 1,
+        "max": 3,
+        "pulse": 0
+    },
+    "speed": {
+        "x": {
+            "min": 0,
+            "max": 0.4
+        },
+        "y": {
+            "min": 0,
+            "max": 0.6
+        }
+    },
+    "colors": {
+        "background": "#344648",
+        "particles": [
+            "#7d8e95",
+            "#ffbb98",
+            "#fbe0c3"
+        ]
+    },
+    "blending": "screen",
+    "opacity": {
+        "center": 1,
+        "edge": 1
+    },
+    // "skew": -2,
+    "shapes": [
+        "c"
+    ]
+});
